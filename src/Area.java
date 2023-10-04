baf1bc759a435b1360710ae2ff33b5de6dc87831
DMGMK DISSANAYAKA <140081823+GayanthikaDissanayaka@users.noreply.github.com>
2023-10-04 13:52:08 +0530

    Labsheet6.1

---
+++ src/Area.java
@@ -0,0 +1,13 @@
+public class Area {
+    double length;
+    double breadth;
+
+    void setDim(double length, double breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+    double getArea() {
+        return length * breadth;
+    }
+}
+
